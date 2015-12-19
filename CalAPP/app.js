ac6369d4d5ac0181282c729d0bf62f011bdb221c
sandeepranga <sandeep.ranga@walkingtree.in>
2015-12-19 14:47:55 +0530

    changes to the Css

--- CalAPP/app.js
+++ CalAPP/app.js
@@ -41,8 +41,9 @@ Ext.application({
                             });
 
                             var calendarPanel = new Ext.Panel({
-                                fullscreen: true,
+                                //fullscreen: true,
                                 layout: 'fit',
+                                title:'CALENDER',
                                 items: [calendarView, {
                                     xtype: 'toolbar',
                                     docked: 'top',
@@ -64,9 +65,95 @@ Ext.application({
                                         handler: function(){
                                             calendarView.setViewMode('day');
                                         }
+                                    },{
+                                        xtype:'button',
+                                        text:'createEvent',
+                                        itemId:'createEvent',
+                                        handler:function(){
+                                            //alert('create New Event');
+                                        }
                                     }]
                                 }]
                             });
+        var nv = Ext.create('Ext.NavigationView', {
+            fullscreen: true,
+            itemId:'navigationView',
+            items: [calendarPanel]
+        });
+
+
+                            calendarView.on('eventtap', function(event){
+
+
+
+                                var compose = Ext.create('CalAPP.view.MyContainer',{
+                                    event:event
+                                });
+                                nv.push(compose);
+
+                            });
+                            calendarView.on('eventdragstart', function(draggable, eventRecord, e){
+                                console.log('eventdragstart');
+                                //console.log(arguments);
+                            });
+                            calendarView.on('beforeeventdrop', function(draggable, eventRecord, e){
+                                console.log('beforeeventdrop');
+                                //console.log(arguments);
+                                return true;
+                            });
+                            calendarView.on('eventdrop', function(draggable, eventRecord, e){
+                                console.log('eventdrop');
+                                //console.log(arguments);
+                            });
+                            calendarView.on('eventdrag', function(draggable, eventRecord, e){
+                                console.log('eventdrag');
+                                //console.log(arguments);
+                            });
+                            calendarView.on('periodchange', function(view, minDate, maxDate, direction){
+                                console.log('periodchange');
+                                console.log(arguments);
+                            });
+                            calendarView.on('selectionchange', function(view, newDate, oldDate){
+
+                               if(view.getViewMode()!="DAY"){
+                                   calendarView.setViewMode('day');
+                                   calendarView.setValue(newDate);
+                                   return;
+                               }
+
+
+                                //Ext.componentQuery.query('createEvent')[0].enable();
+                                //Ext.componentQuery.query('createEvent')[0].presentSelectedDate = newDate ;
+                                //debugger;
+                            });
+                            calendarView.on('eventdragstart', function(draggable, eventRecord, e){
+                                console.log('eventdragstart');
+                                //console.log(arguments);
+                            });
+                            calendarView.on('beforeeventdrop', function(draggable, eventRecord, e){
+                                console.log('beforeeventdrop');
+                                //console.log(arguments);
+                                return true;
+                            });
+                            calendarView.on('eventdrop', function(draggable, eventRecord, e){
+                                console.log('eventdrop');
+                                //console.log(arguments);
+                            });
+                            calendarView.on('eventdrag', function(draggable, eventRecord, e){
+                                console.log('eventdrag');
+                                //console.log(arguments);
+                            });
+                            calendarView.on('periodchange', function(view, minDate, maxDate, direction){
+                                console.log('periodchange');
+                                console.log(arguments);
+                            });
+                            calendarView.on('selectionchange', function(view, newDate, oldDate){
+                                console.log('selectionchange');
+                                console.log(arguments);
+                            });
+
+
+
     }
 
 });
--- CalAPP/app/view/MyContainer.js
+++ CalAPP/app/view/MyContainer.js
@@ -16,7 +16,83 @@
 Ext.define('CalAPP.view.MyContainer', {
     extend: 'Ext.Container',
 
+    requires: [
+        'Ext.field.Text',
+        'Ext.Toolbar',
+        'Ext.Button'
+    ],
+
     config: {
+        height: '100%',
+        width: '100%',
+        layout: 'fit',
+        items: [
+            {
+                xtype: 'textfield',
+                docked: 'top',
+                label: 'To'
+            },
+            {
+                xtype: 'container',
+                html: '<div id="tymca" ></div>',
+                itemId: 'tinyMCAEditor',
+                width: '100%',
+                listeners: [
+                    {
+                        fn: function(element, eOpts) {
+                            tinymce.init({
+                                selector: '#tymca',
+                                //height: 500,
+                                theme: 'modern',
+                                plugins: [
+                                'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                                'searchreplace wordcount visualblocks visualchars code fullscreen',
+                                'insertdatetime media nonbreaking save table contextmenu directionality',
+                                'emoticons template paste textcolor colorpicker textpattern imagetools'
+                                ],
+                                toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                                toolbar2: 'print preview media | forecolor backcolor emoticons',
+                                image_advtab: true,
+                                templates: [
+                                { title: 'Test template 1', content: 'Test 1' },
+                                { title: 'Test template 2', content: 'Test 2' }
+                                ],
+                                content_css: [
+                                '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+                                '//www.tinymce.com/css/codepen.min.css'
+                                ]
+                            });
+
+                        },
+                        event: 'painted'
+                    }
+                ]
+            },
+            {
+                xtype: 'toolbar',
+                docked: 'bottom',
+                items: [
+                    {
+                        xtype: 'button',
+                        handler: function(button, e) {
+                            this.up('#navigationView').pop();
+
+                        },
+                        text: 'Schedule'
+                    }
+                ]
+            }
+        ]
+    },
+
+    initialize: function() {
+        // tinymce.init({
+        //   selector: '#tymca',
+
+
+
+
+        this.callParent();
     }
 
 });
